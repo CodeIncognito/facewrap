4522259e61992d712de45d76ac670656d823990e
capn-neo <neo@codeincognito.com>
2013-09-08 04:10:41 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facewrap::Application.config.secret_key_base = 'bc9b4fda8b5f79e46130209b5ad2e2bc554288bc7b82174aeceb13e246f6e14da293f83cacbd948156584e789f2637e0b97cb6f93842b225c2616377b48ec681'
